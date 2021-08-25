fba03729036699b2b379e5d03afa6538211e36a0
Sami Ullah <muftisami2010gmail.com>
2021-08-25 20:13:35 +0530

    some fixes

--- website/src/Helper/Cons.jsx
+++ website/src/Helper/Cons.jsx
@@ -1,2 +1,2 @@
 export const LIVE_API_URL = 'https://admin.crazyforstudy.com/api/v1/'
-export const LOCAL_API_URL = 'http://localhost:8000/api/v1/'
+export const LOCAL_API_URL = 'http://localhost:3000/api/v1/'
